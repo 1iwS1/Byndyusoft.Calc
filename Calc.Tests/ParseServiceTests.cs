3c604c5d362e470b3f16180b6af1a85ed433703c
iwS <msp1108@mail.ru>
2024-08-15 17:11:28 +0200

    create more Test for ParseSourceExpression process

--- Calc.Tests/ParseServiceTests.cs
+++ Calc.Tests/ParseServiceTests.cs
@@ -11,7 +11,7 @@ namespace Calc.Tests
   {
     [Theory]
     [InlineData("6+7")]
-    public void GetInfixExpression_InputString_ReturnQueueOfIExpresions(string value)
+    public void GetInfixExpression_InputString6plus7_ReturnQueueOfIExpresions(string value)
     {
       // Arrange
       IParseProcess parser = new ParseSourceExpressionService();
@@ -25,7 +25,53 @@ namespace Calc.Tests
       var result = parser.GetInfixExpression(value);
 
       // Assert
-      Assert.Equal(elements, result);
+      Assert.StrictEqual(elements, result);
+    }
+
+    [Theory]
+    [InlineData("6+7*9")]
+    public void GetInfixExpression_InputString6plus7multiply9_ReturnQueueOfIExpresions(string value)
+    {
+      // Arrange
+      IParseProcess parser = new ParseSourceExpressionService();
+
+      Queue<IExpressionElement> elements = new();
+      elements.Enqueue(new Operand() { Value = 6 });
+      elements.Enqueue(new PlusOperator());
+      elements.Enqueue(new Operand() { Value = 7 });
+      elements.Enqueue(new MultiplierOperator());
+      elements.Enqueue(new Operand() { Value = 9 });
+
+      // Act
+      var result = parser.GetInfixExpression(value);
+
+      // Assert
+      Assert.StrictEqual(elements, result);
+    }
+
+    [Theory]
+    [InlineData("-4/2+7-(-9)")]
+    public void GetInfixExpression_InputString_ReturnQueueOfIExpresions(string value)
+    {
+      // Arrange
+      IParseProcess parser = new ParseSourceExpressionService();
+
+      Queue<IExpressionElement> elements = new();
+      elements.Enqueue(new Operand() { Value = -4 });
+      elements.Enqueue(new DivisionOperator());
+      elements.Enqueue(new Operand() { Value = 2 });
+      elements.Enqueue(new PlusOperator());
+      elements.Enqueue(new Operand() { Value = 7 });
+      elements.Enqueue(new MinusOperator());
+      elements.Enqueue(new LeftParenthesis());
+      elements.Enqueue(new Operand() { Value = -9 });
+      elements.Enqueue(new RightParenthesis());
+
+      // Act
+      var result = parser.GetInfixExpression(value);
+
+      // Assert
+      Assert.StrictEqual(elements, result);
     }
   }
 }
