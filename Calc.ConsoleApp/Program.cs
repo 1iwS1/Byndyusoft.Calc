fecf7c238e41c8d6c2ae84477272796de9cce48f
iwS <msp1108@mail.ru>
2024-08-16 14:53:00 +0200

    add realization of Main method

--- Calc.ConsoleApp/Program.cs
+++ Calc.ConsoleApp/Program.cs
@@ -8,6 +8,50 @@ public class Program
 {
   public static int Main(string[] args)
   {
-    throw new NotImplementedException();
+    IServiceCollection services = new ServiceCollection();
+    services.AddServices();
+
+    var serviceProvider = services.BuildServiceProvider();
+
+    string? sourceExpression = string.Empty;
+
+    try
+    {
+      var consoleProvider = serviceProvider.GetRequiredService<IProvider>();
+      sourceExpression = args[0];
+
+      if (sourceExpression == null)
+      {
+        throw new ArgumentException("Nothing to calculate...");
+      }
+    }
+
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.ToString());
+      return -1;
+    }
+
+    try
+    {
+      var parser = serviceProvider.GetRequiredService<IParseProcess>();
+      var infixForm = parser.GetInfixExpression(sourceExpression);
+
+      var converter = serviceProvider.GetRequiredService<IConverter>();
+      var postfixForm = converter.GetPostfixExpression(infixForm);
+
+      var calculater = serviceProvider.GetRequiredService<ICalculateService>();
+      double result = calculater.GetResultOfExpression(postfixForm);
+
+      Console.WriteLine("\n" + "The result is " + result);
+    }
+
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.ToString());
+      return -1;
+    }
+
+    return 1;
   }
 }
